a395819a99f63f4d416857e4683602f2a182ff8d
#51978077 <itachiut1r4@gmail.com>
2020-12-06 17:08:27 -0300

    Warning

--- src/main.py
+++ src/main.py
@@ -302,15 +302,17 @@ def predict(model):
     )
 
 
+#still not working
+def use_GPU():
+  print(device_lib.list_local_devices())
+
+  #physical_devices = tf.config.experimental.list_physical_devices('GPU')
+  print("Num GPUs Available: ", tf.config.experimental.list_physical_devices('GPU'))
+  #tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-print(device_lib.list_local_devices())
-
-#physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print("Num GPUs Available: ", tf.config.experimental.list_physical_devices('GPU'))
-#tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-
+print(tf.__version__)
+#use_GPU()
 
 while True:
 
